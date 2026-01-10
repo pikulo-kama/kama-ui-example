eba54da86f763aa468a678a4370236463e8168f8
Artur Drabazha <jhorr2222@gmail.com>
2026-01-10 15:08:40 +0200

    Initial commit

---
+++ kexample/controller/button.py
@@ -0,0 +1,38 @@
+from kui.component.button import KamaPushButton
+from kui.core.controller import WidgetController
+from kui.core.shortcut import add_dynamic_data, dynamic_data
+
+
+class IncrementButtonController(WidgetController):
+
+    def setup(self, button: KamaPushButton):
+
+        def on_click():
+            counter_value = dynamic_data("counter") or 0
+            add_dynamic_data("counter", counter_value + 1)
+
+            self.manager.event_refresh("counter_value_change")
+
+        button.clicked.connect(on_click)
+
+
+class DecrementButtonController(WidgetController):
+
+    def setup(self, button: KamaPushButton):
+        def on_click():
+            counter_value = dynamic_data("counter") or 0
+            add_dynamic_data("counter", max(counter_value - 1, 0))
+
+            self.manager.event_refresh("counter_value_change")
+
+        button.clicked.connect(on_click)
+
+
+class ResetButtonController(WidgetController):
+
+    def setup(self, button: KamaPushButton):
+        def on_click():
+            add_dynamic_data("counter", 0)
+            self.manager.event_refresh("counter_value_change")
+
+        button.clicked.connect(on_click)
---
+++ kexample/main.py
@@ -0,0 +1,6 @@
+import sys
+from kui.core.app import KamaApplication
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(KamaApplication().exec())
