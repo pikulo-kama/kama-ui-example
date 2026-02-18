a377b9f56ed8bc1880da6776b461e4128fac80b3
Artur Drabazha <jhorr2222@gmail.com>
2026-02-18 22:33:15 +0200

    Update project to stay up-to-date with framework

--- src/kexample/controller/button.py
+++ src/kexample/controller/button.py
@@ -1,7 +1,7 @@
 from kui.component.button import KamaPushButton
 from kui.core.controller import WidgetController
 from kui.core.metadata import ControllerArgs
-from kui.core.shortcut import add_dynamic_data, dynamic_data
+from kui.core.shortcut import add_dynamic_data, dynamic_data, tr
 
 
 class IncrementButtonController(WidgetController):
@@ -9,10 +9,19 @@ class IncrementButtonController(WidgetController):
     def setup(self, button: KamaPushButton, args: ControllerArgs):
 
         def on_click():
-            counter_value = dynamic_data("counter") or 0
-            add_dynamic_data("counter", counter_value + 1)
+            counter_value = (dynamic_data("counter") or 0) + 1
+            pseudo_mode_active = dynamic_data("isPseudoModeActivated")
+            add_dynamic_data("counter", counter_value)
 
-            self.manager.event_refresh("counter_value_change")
+            if counter_value == 11 and not pseudo_mode_active:
+                add_dynamic_data("isPseudoModeActivated", True)
+                self.application.translations.locale = "en_PSEUDO"
+                self.manager.refresh()
+
+                self.application.window.notification(tr("message_PseudoModeOn"))
+
+            else:
+                self.manager.event_refresh("counter_value_change")
 
         button.clicked.connect(on_click)
 
@@ -34,6 +43,16 @@ class ResetButtonController(WidgetController):
     def setup(self, button: KamaPushButton, args: ControllerArgs):
         def on_click():
             add_dynamic_data("counter", 0)
-            self.manager.event_refresh("counter_value_change")
+            pseudo_mode_active = dynamic_data("isPseudoModeActivated")
+
+            if pseudo_mode_active:
+                self.application.translations.locale = None
+                add_dynamic_data("isPseudoModeActivated", False)
+                self.manager.refresh()
+
+                self.application.window.notification(tr("message_PseudoModeOff"))
+
+            else:
+                self.manager.event_refresh("counter_value_change")
 
         button.clicked.connect(on_click)
